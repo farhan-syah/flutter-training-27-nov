6a2b022062c4d544a0a7181f7e9219966db4844a
Farhan Syah <bizimpulse@gmail.com>
2021-11-27 15:01:22 +0800

    Add stateful widget of label containers

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentCategoryIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -37,9 +42,101 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('Test Text'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 20, bottom: 20),
+            child: Text(
+              'Find modern\nfurniture for you',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Padding(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            padding: EdgeInsets.only(left: 20, right: 10),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                categories.length,
+                (index) => Label(
+                  label: categories[index],
+                  active: currentCategoryIndex == index,
+                  onTap: () {
+                    currentCategoryIndex = index;
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class Label extends StatelessWidget {
+  final String label;
+  final bool active;
+  final Function onTap;
+  Label({required this.label, required this.active, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Material(
+        borderRadius: BorderRadius.circular(30),
+        clipBehavior: Clip.hardEdge,
+        color: active ? Colors.black : Colors.grey.shade50,
+        child: InkWell(
+          child: Container(
+            width: 70,
+            height: 30,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border:
+                  Border.all(color: active ? Colors.transparent : Colors.grey),
+            ),
+            child: Center(
+              child: Text(
+                label,
+                style: TextStyle(color: active ? Colors.white : Colors.grey),
+              ),
+            ),
+          ),
+          onTap: () {
+            onTap();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+List<String> categories = [
+  'Chair',
+  'Sofa',
+  'Table',
+  'Lamp',
+  'Kitchen',
+  'Office'
+];
